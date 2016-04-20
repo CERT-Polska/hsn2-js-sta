22433c3e3ccd8e50fc0b72876e4a7fbf2fd429ad
Marek Bajor <marek.bajor@nask.pl>
2016-04-20 08:48:09 +0200

    Minor fixes

--- src/main/java/pl/nask/hsn2/service/SSDeepHash.java
+++ src/main/java/pl/nask/hsn2/service/SSDeepHash.java
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) NASK, NCSC
- * 
+ *
  * This file is part of HoneySpider Network 2.0.
- * 
+ *
  * This is a free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -25,16 +25,16 @@ import java.util.StringTokenizer;
 public class SSDeepHash implements Comparable<SSDeepHash>{
 	private final int match;
 	private final String hash;
-	
+
 	public SSDeepHash(int match, String hash) {
 		this.match = match;
 		this.hash = hash;
 	}
-	
+
 	public SSDeepHash(String readLine) {
 		String line = readLine.trim();
 		StringTokenizer tokenizer = new StringTokenizer(line);
-		
+
 		int count = tokenizer.countTokens();
 		if(count == 2){
 			match = Integer.valueOf(tokenizer.nextToken());
@@ -57,13 +57,21 @@ public class SSDeepHash implements Comparable<SSDeepHash>{
 	public final int compareTo(SSDeepHash o) {
 		return match - o.match;
 	}
-	
+
 	@Override
 	public final boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
 		if (!(o instanceof SSDeepHash)) {
 			return false;
 		}
 		SSDeepHash s = (SSDeepHash) o;
 		return match == s.match && hash.equals(s.hash);
+	}
+
+	@Override
+	public final int hashCode() {
+		return hash.hashCode();
 	}
 }
